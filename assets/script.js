c5c809700a99e0873dda010294071f309d911a97
Marlena Hooker Moore <132782689+Ms-Marlena@users.noreply.github.com>
2023-07-22 15:26:32 -0400

    connected to CurrentWeatherAPI

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,70 @@
 var apiKey = "212be1e5713240df908c291b8fbba3f8";
-var citySta te = document.getElementById("cityState");
+var city = document.getElementById("city");
 
 //event Listener to search city
 document.getElementById("search").addEventListener("click", function () {
-    search(cityState.value);
-
+    searchCurrent(city.value);
 })
 
 //function to search by city
-function search(city) {
-    var requestURL =
+function searchCurrent(city) {
+    var requestCurrentURL =
       "https://api.openweathermap.org/data/2.5/weather?q=" +
       city +
       "&appid=" +
       apiKey +
       "&units=imperial";
     
-    fetch(requestURL)
+    fetch(requestCurrentURL)
         .then((response) => {
             console.log(response);
             return response.json();
-        }).then((data) => {
-            console.log(data);
-    })
+        }).then((currentData) => {
+            console.log(currentData);
+            console.log(currentData.coord.lat);
+            console.log(currentData.coord.lon);
+            console.log(
+            currentData.main.temp)
+            
+        })
+
+    // let cityLat = currentData.coord.lat;
+    // let cityLon = currentData.coord.lon;
+    // console.log(cityLat, cityLon); 
 }
+
+// function displayCurrentWeather(city) {
+    
+// }
+
+
+// Getting city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed. Also coordinates to give to the 5-day thing???
+
+// new WeatherData(
+//   data.current.temp,
+//   weatherToday.weather[0].icon.replace("n", "d"),
+//   weatherToday.weather[0].description,
+//   data.daily[0].temp.min,
+//   data.daily[0].temp.max
+// );
+//5-day weather search
+// function search(city) {
+//     var requestFiveDayURL =
+//         "https://api.openweathermap.org/data/2.5/forecast? +
+//         "lat =" +
+//         { lat } +
+//         "& lon=" +
+//         { lon } +
+//         "& appid=" +
+//         apiKey +
+//         "&units=imperial" +
+    
+//     fetch(requestFiveDayURL)
+//         .then((response) => {
+//             console.log(response);
+//             return response.json();
+//         }).then((forecastData) => {
+//             console.log(forecastData);
+        
+//     })
+// }
