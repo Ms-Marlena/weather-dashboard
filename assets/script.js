411098918cd91c4fbd7132cf14f1c85bbb1b8317
Marlena Hooker Moore <132782689+Ms-Marlena@users.noreply.github.com>
2023-07-22 13:41:30 -0400

    started html, css,  and js files

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+var apiKey = "212be1e5713240df908c291b8fbba3f8";
+var cityState = document.getElementById("cityState");
+
+//event Listener to search city
+document.getElementById("search").addEventListener("click", function () {
+    search(cityState.value);
+
+})
+
+//function to search by city
+function search(city) {
+    var requestURL =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&appid=" +
+      apiKey +
+      "&units=imperial";
+    
+    fetch(requestURL)
+        .then((response) => {
+            console.log(response);
+            return response.json();
+        }).then((data) => {
+            console.log(data);
+    })
+}
