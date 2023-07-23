c1d2e8416a89bdf54629a744d83d9f2788c86ead
Marlena Hooker Moore <132782689+Ms-Marlena@users.noreply.github.com>
2023-07-23 14:27:07 -0400

    added: lat + lon from current; got 5-day forecast

--- assets/script.js
+++ assets/script.js
@@ -1,70 +1,55 @@
-var apiKey = "212be1e5713240df908c291b8fbba3f8";
+var apiKey = "cc6e371f3d60a75d44278351a03a214b";
 var city = document.getElementById("city");
 
 //event Listener to search city
 document.getElementById("search").addEventListener("click", function () {
-    searchCurrent(city.value);
-})
+  searchCurrent(city.value);
+});
 
 //function to search by city
 function searchCurrent(city) {
     var requestCurrentURL =
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-      city +
-      "&appid=" +
-      apiKey +
-      "&units=imperial";
-    
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&appid=" +
+        apiKey +
+        "&units=imperial";
+
     fetch(requestCurrentURL)
         .then((response) => {
             console.log(response);
             return response.json();
-        }).then((currentData) => {
-            console.log(currentData);
+        })
+        .then((currentData) => {
             console.log(currentData.coord.lat);
             console.log(currentData.coord.lon);
-            console.log(
-            currentData.main.temp)
-            
-        })
+            document.querySelector("#currentWeather").textContent =
+              `${currentData.name} Today: Temperature: ${currentData.main.temp} F Humidity: ${currentData.main.humidity}%, Wind Speed: ${currentData.wind.speed} mph`;
 
-    // let cityLat = currentData.coord.lat;
-    // let cityLon = currentData.coord.lon;
-    // console.log(cityLat, cityLon); 
-}
+            var cityLat = currentData.coord.lat;
+            var cityLon = currentData.coord.lon;
 
-// function displayCurrentWeather(city) {
+            var requestForecastURL =
+                "https://api.openweathermap.org/data/2.5/forecast?lat=" +
+                cityLat +
+                "&lon=" +
+                cityLon +
+                "&appid=" +
+                apiKey +
+                "&units=imperial";
     
-// }
-
-
-// Getting city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed. Also coordinates to give to the 5-day thing???
-
-// new WeatherData(
-//   data.current.temp,
-//   weatherToday.weather[0].icon.replace("n", "d"),
-//   weatherToday.weather[0].description,
-//   data.daily[0].temp.min,
-//   data.daily[0].temp.max
-// );
-//5-day weather search
-// function search(city) {
-//     var requestFiveDayURL =
-//         "https://api.openweathermap.org/data/2.5/forecast? +
-//         "lat =" +
-//         { lat } +
-//         "& lon=" +
-//         { lon } +
-//         "& appid=" +
-//         apiKey +
-//         "&units=imperial" +
+            fetch(requestForecastURL)
     
-//     fetch(requestFiveDayURL)
-//         .then((response) => {
-//             console.log(response);
-//             return response.json();
-//         }).then((forecastData) => {
-//             console.log(forecastData);
-        
-//     })
-// }
+            .then((response) => {
+                    console.log(response);
+                    return response.json();
+                }).then((forecastData) => {
+                    console.log(forecastData);
+                    document.querySelector("#foreCast2").innerHTML = "HALP!!!";
+                    document.querySelector("#foreCast3").innerHTML = "HALP!!!";
+                    document.querySelector("#foreCast4").innerHTML = "HALP!!!";
+                    document.querySelector("#foreCast5").innerHTML = "HALP!!!";
+
+                });
+        });
+};
